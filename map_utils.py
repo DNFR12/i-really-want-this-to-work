41e8359a97b002c77d32aa13a1ef04f6926fde63
DNFR12 <mcgarrype@gmail.com>
2025-08-07 15:53:38 -0500

    Update map_utils.py

--- map_utils.py
+++ map_utils.py
@@ -1,16 +1,31 @@
 import folium
+from geopy.geocoders import Nominatim
 
-def create_route_map(shipment_type, origin_coords, dest_coords):
-    route_map = folium.Map(location=origin_coords, zoom_start=5)
+geolocator = Nominatim(user_agent="freight-app")
 
-    if shipment_type == "OTR Bulk":
-        folium.PolyLine([origin_coords, dest_coords], color="blue", weight=5, tooltip="Road Route").add_to(route_map)
-    elif shipment_type == "Iso Tank Bulk":
-        folium.PolyLine([origin_coords, dest_coords], color="green", weight=5, tooltip="Rail Route").add_to(route_map)
-    elif shipment_type in ["Containers Freight", "LTL & FTL"]:
-        folium.PolyLine([origin_coords, dest_coords], color="purple", weight=3, dash_array="5, 10", tooltip="Ocean Route").add_to(route_map)
+def geocode(city):
+    location = geolocator.geocode(city)
+    return (location.latitude, location.longitude) if location else (None, None)
 
-    folium.Marker(origin_coords, tooltip="Origin", icon=folium.Icon(color="green")).add_to(route_map)
-    folium.Marker(dest_coords, tooltip="Destination", icon=folium.Icon(color="red")).add_to(route_map)
+def create_map(origin=None, destination=None, shipment_type=None):
+    # Default location: central U.S.
+    fmap = folium.Map(location=[39.5, -98.35], zoom_start=4)
 
-    return route_map
+    if origin and destination:
+        orig_coords = geocode(origin)
+        dest_coords = geocode(destination)
+
+        if orig_coords and dest_coords:
+            folium.Marker(orig_coords, tooltip='Origin: ' + origin).add_to(fmap)
+            folium.Marker(dest_coords, tooltip='Destination: ' + destination).add_to(fmap)
+
+            if shipment_type == "OTR Bulk":
+                folium.PolyLine([orig_coords, dest_coords], color='blue', weight=4).add_to(fmap)
+            elif shipment_type == "Iso Tank Bulk":
+                folium.PolyLine([orig_coords, dest_coords], color='green', weight=4, dash_array='10').add_to(fmap)
+            elif shipment_type == "Containers Freight":
+                folium.PolyLine([orig_coords, dest_coords], color='purple', weight=3, dash_array='1').add_to(fmap)
+            elif shipment_type == "LTL FTL":
+                folium.PolyLine([orig_coords, dest_coords], color='orange', weight=2, dash_array='5').add_to(fmap)
+
+    return fmap._repr_html_()
